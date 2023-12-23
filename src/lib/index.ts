853468b6a51e43634f10035efb61cea9fe6532a5
TheBharatMishra <mishrabm2004@gmail.com>
2023-12-23 20:48:31 +0530

    To Be Continued

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+const servers = {iceservers} 
+
+
+
+//Global State
+let pc = new RTCPeerConnection(servers)
+let localStream = null
+let remoteStream = null
