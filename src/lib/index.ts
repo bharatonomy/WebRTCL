ce5c88e08bf33f45bd281a820c94f217663a7002
TheBharatMishra <mishrabm2004@gmail.com>
2024-01-13 16:01:26 +0530

    WIP

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,25 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-const servers = {iceservers} 
-
-
+const servers = {
+	iceServers: [{ urls: ['stun:stun4.l.google.com:19302', 'stun:stun3.l.google.com:19302'] }],
+	iceCandidatePoolSize: 10
+};
 
 //Global State
-let pc = new RTCPeerConnection(servers)
-let localStream = null
-let remoteStream = null
+let pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+// Frontend components.
+const webcamVideo = document.getElementById('webcamVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const webcamButton = document.getElementById('webcamButton');
+const answerButton = document.getElementById('answerButton');
+const hangupButton = document.getElementById('hangupButton');
+
+// Setup media resources
+webcamButton?.onclick = async () => {
+	localStream = await navigator;
+};
