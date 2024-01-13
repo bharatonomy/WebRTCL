d7bb1966e92642414f1c8681787a72fd59b88c5a
TheBharatMishra <mishrabm2004@gmail.com>
2024-01-13 16:00:13 +0530

    FrontEnd Structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Start your webcam</h2>
+<div class="videos">
+    <span><h3>Local</h3><video id="webcamVideo" autoplay playsinline></video></span>
+    <span><h3>Remote</h3><video id="remoteVideo" autoplay playsinline></video></span>
+</div>
+
+<button id="webcamButton">Start Webcam</button>
+<h3>Create a new Call</h3>
+<button id="callButton" disabled>Call</button>
+
+<h3>Join a Call</h3>
+<input type="text" name="" id="callInput">
+<button id="answerButton" disabled>Answer</button>
+<button id="hangupButton">Start Webcam</button>
+
+<script type="module" src="../lib/index.ts"></script>
