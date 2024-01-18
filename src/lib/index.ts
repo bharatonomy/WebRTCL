f8b242de5a5e2b81eb7fd9acbc2723680f3395c0
TheBharatMishra <mishrabm2004@gmail.com>
2024-01-18 19:13:10 +0530

    WIP

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -21,5 +21,22 @@ const hangupButton = document.getElementById('hangupButton');
 
 // Setup media resources
 webcamButton?.onclick = async () => {
-	localStream = await navigator;
+	localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+	remoteStream = new MediaStream();
+
+	// Push tracks from local stream to peer connection
+	localStream.getTracks().forEach((track) => {
+		pc.addTrack(track, localStream);
+	});
+
+	// Pull tracks from the remote stream, add to video stream
+	pc.ontrack = (event) => {
+		event.streams[0].getTracks().forEach((track) => remoteStream.addTrack(track));
+	};
+
+	webcamVideo.srcObject = localStream;
+	remoteVideo.srcObject = remoteStream;
 };
+
+// Create an offer
+callButton?.onclick = async (params: type) => {};
